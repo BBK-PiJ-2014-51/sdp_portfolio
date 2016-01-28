37d177ed6d7e58d0444612fe64dc4f1eff33d73a
unknown <cclayt02@CSPC060.dcs.bbk.ac.uk>
2016-01-28 19:24:32 +0000

    added the first part of week 4 exercises

---
+++ weekly exercises/week 4/src/Repl.scala
@@ -0,0 +1,17 @@
+
+/**
+ * @author cclayt02
+ */
+object WeekFour extends App{
+  val value = 17
+  println(value)
+  //value = 20 //can't reassign
+  
+  val str = "ABC1234"
+  println(str)
+  //str = "DEF1234" //can't reassign
+  
+  val dec = 15.56
+  println(dec)
+  
+}
