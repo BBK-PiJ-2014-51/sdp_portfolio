47477c661f225510ab7f9d844542e500a30d63c2
caleb <clayton.caleb@gmail.com>
2016-02-10 01:18:38 +0000

    did some work on composite pattern from week 5

---
+++ weekly exercises/week 5/src/composite/HtmlElement.java
@@ -0,0 +1,33 @@
+package composite;
+
+public class HtmlElement extends HtmlTag {
+
+	public HtmlElement(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getTagName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setStartTag(String tag) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setEndTag(String tag) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void generateHtml() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ weekly exercises/week 5/src/composite/HtmlParentElement.java
@@ -0,0 +1,33 @@
+package composite;
+
+public class HtmlParentElement extends HtmlTag {
+
+	public HtmlParentElement(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getTagName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setStartTag(String tag) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setEndTag(String tag) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void generateHtml() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
