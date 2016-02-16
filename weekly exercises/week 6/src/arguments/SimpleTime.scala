a1eb4c891f350fdd43be0bfffd699e12ca9a8891
caleb <clayton.caleb@gmail.com>
2016-02-16 00:52:07 +0000

    added num 6 from week 6- simple time

--- weekly exercises/week 6/src/arguments/SimpleTime.scala
+++ weekly exercises/week 6/src/arguments/SimpleTime.scala
@@ -1,8 +1,17 @@
 package arguments
+import com.atomicscala.AtomicTest._
 
 /**
  * @author cclayt02
  */
-class SimpleTime(hours: Int, minutes: Int) {
+class SimpleTime(val hours: Int, val minutes: Int) {
   
+  
+}
+
+
+object TestRunner extends App{
+  val t = new SimpleTime(hours=5, minutes=30)
+  t.hours is 5
+  t.minutes is 30
 }
