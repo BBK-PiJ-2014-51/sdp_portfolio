906e162ddfecf414803329d29ceddac678498c94
caleb <clayton.caleb@gmail.com>
2016-04-10 14:21:06 +0100

    renamed weekly exercises to exercises as per suggested repo structure on moodle

--- exercises/week 6/src/constructors/ClothesWasher.scala
+++ exercises/week 6/src/constructors/ClothesWasher.scala
@@ -15,7 +15,7 @@ class ClothesWasher2(modelName: String = "def_name", capacity: Double = 0.2d) {
 
 class ClothesWasher3(modelName: String = "def_name", capacity: Double = 0.2d) {
   def this(name:String){
-    1 + 2 //this won't compile
+    //1 + 2 //this won't compile
     this(modelName = name)
   }
   def this(cap: Double){
--- exercises/week 6/src/constructors/Tea.scala
+++ exercises/week 6/src/constructors/Tea.scala
@@ -24,24 +24,26 @@ class Tea(name: String = "Earl Grey", decaf: Boolean = false, milk: Boolean = fa
       numCals += 16
      
       numCals
-    
   }
   
 }
 
 
 object TeaTest extends App{
+    
     val tea = new Tea
     tea.describe is "Earl Grey"
     tea.calories is 0
+    
     val lemonZinger = new Tea(decaf = true, name="Lemon Zinger")
     lemonZinger.describe is "Lemon Zinger decaf"
     lemonZinger.calories is 0
+    
     val sweetGreen = new Tea( name="Jasmine Green", sugar=true)
     sweetGreen.describe is "Jasmine Green + sugar"
     sweetGreen.calories is 16
+    
     val teaLatte = new Tea(sugar=true, milk=true)
     teaLatte.describe is "Earl Grey + milk + sugar"
     teaLatte.calories is 116
-  
 }
